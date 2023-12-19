e978e022fd4730ef85eb37405516214495aaeae7
frenkielm <Administrator@XZ-LND50B6N1VSJ>
2023-12-19 10:51:21 +0800

    23-12-19 10:51

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -41,8 +41,8 @@ class Cutout:
 transform_augmented = T.Compose([
     T.RandomHorizontalFlip(),
     T.RandomCrop(32, padding=4),
+    T.ToTensor(),
     Cutout(n_holes=1, length=16),
-    T.ToTensor()
 ])
 
 class ChunkSampler(sampler.Sampler):
